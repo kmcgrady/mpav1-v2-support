4346e2d87a59d0dff87ee9402b67f48b5a08b7fa
Ken McGrady <ken.mcgrady@snowflake.com>
2025-02-12 11:48:06 -0800

    Revert "Move hidden sidebar to main"
    This reverts commit 832471d31f17c815e660e3e9dd804402e5d5d644.

---
+++ single-page-app-not-found/app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.write("## Repro Steps")
+st.write("Point browser to a specific page that doesn't exist")
+st.write("## Expected Result")
+st.write("Page not found dialog should be displayed")
+st.write("## Actual Result")
+st.write("Page not found dialog is not displayed. It just runs the main page")
