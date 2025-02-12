ad482db2d02bb8ad652fb5a54e263240ac89e709
Ken McGrady <ken.mcgrady@snowflake.com>
2025-02-12 11:37:43 -0800

    Show app

--- pages/page2.py
+++ pages/page2.py
@@ -3,6 +3,7 @@ import streamlit as st
 # Note the sidebar is requested to be hidden, so we populate the sidebar
 # with page links instead.
 with st.sidebar:
+  st.page_link("app.py")
   st.page_link("pages/page1.py")
   st.page_link("pages/page2.py")
   st.page_link("pages/page3.py")
--- pages/page3.py
+++ pages/page3.py
@@ -3,6 +3,7 @@ import streamlit as st
 # Note the sidebar is requested to be hidden, so we populate the sidebar
 # with page links instead.
 with st.sidebar:
+  st.page_link("app.py")
   st.write("Some content for Page 3")
 
 st.write("# Page 3")
